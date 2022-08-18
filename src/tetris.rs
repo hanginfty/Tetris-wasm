36f5741a7d0e81a02dd3e1e7f0518b2e6db68e3f
hanginfty <2263126765@qq.com>
2022-08-18 21:55:57 +0800

    feat: Block definition

--- src/tetris.rs
+++ src/tetris.rs
@@ -1,10 +1,36 @@
+use std::collections::HashSet;
+
 use crate::block::Block;
 
 pub struct Tetris {
     width: i32,
     height: i32,
-    curr_block: Block,
+    cur_block: Block,
     fixed_block: Vec<Block>,
+    is_lost: bool,
 }
 
-impl Tetris {}
+impl Tetris {
+    pub fn new(width: i32, height: i32) -> Self {
+        Self {
+            width,
+            height,
+            // cur_block: &Block::new_rand() + Pos(width / 2, 0),
+            cur_block: todo!(),
+            fixed_block: vec![],
+            is_lost: false,
+        }
+    }
+
+    pub fn is_line_full(&self, y: i32) -> bool {
+        self.fixed_block
+            .iter()
+            .flat_map(|b| b.iter_positions())
+            .filter(|pos| pos.1 == y)
+            .collect::<HashSet<_>>()
+            .len() as i32
+            == self.width
+    }
+
+    fn remove_line(&mut self, y: i32) {}
+}
